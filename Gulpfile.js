e6ae6f3770c58e73bcff53c514cd8afa22ee6aeb
Rane Gridley <ranegri3@gmail.com>
2016-04-04 12:43:00 -0700

    Add module/dependency packages and gulp
    Includes (with expected dependencies):
    bower.json
    package.json
    Gulpfile
    eslint for HR styling
    all .ingore files

---
+++ Gulpfile.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var gulp = require('gulp');
+var sync = require('browser-sync');
+var nodemon = require('gulp-nodemon');
+
+var paths = {
+  scripts: ['client/app/**/*.js'],
+  html: ['client/app/**/*.html', 'client/index.html'],
+  styles: ['client/styles/style.css']
+};
+
+gulp.task('start', ['serve'], function () {
+  sync({
+    notify: true,
+    injectChanges: true,
+    files: paths.scripts.concat(paths.html, paths.styles),
+    proxy: 'localhost:8000'
+  });
+});
+
+gulp.task('serve', function () {
+  nodemon({
+    script: './server/server.js',
+    ignore: 'node_modules/**/*.js'
+  });
+});
+
+gulp.task('default', ['start']);
